f50e6a1df0c28cc3e8fcb8dca19b2167c46fd2a5
lmstrand <lmstrand@gmail.com>
2024-03-25 15:40:26 +0200

    Update js script

--- Koha/Plugin/Fi/KohaSuomi/CheckoutReportLink/script.js
+++ Koha/Plugin/Fi/KohaSuomi/CheckoutReportLink/script.js
@@ -1,17 +1,28 @@
 /// ALKU ///
 // Linkki lainatilastoraporttiin tietueen pääsivulla
 $(document).ready(function() {
-    if (window.location.pathname == '/cgi-bin/koha/catalogue/detail.pl') { 
-      $('#catalogue_detail_biblio div').append('<span class="results_summary"><span class="lainatiedot label">Lainaustilastot: </span><a id="lainatiedot" href="#">Raportti</a></span>');
-    }  
-  });
-  
-  $('body').on('click', '#lainatiedot', function(e) {
-    e.preventDefault();
-      var reportnumber = REPLACE_BY_CONFIG_PARAM_A;   
-      var params = new URLSearchParams(window.location.search);
-      var biblionumber = params.get('biblionumber');
-      var reporturl = '/cgi-bin/koha/reports/guided_reports.pl?reports=' + reportnumber + '&phase=Run+this+report&param_name=biblionumber&sql_params=' + biblionumber;
-      var newin = window.open(reporturl,'popup','width=1024,height=768,resizable=no,toolbar=false,scrollbars=yes,top');
-  });
-  /// LOPPU ///
+  if (window.location.pathname == '/cgi-bin/koha/catalogue/detail.pl') { 
+
+    // Kielivalinta
+    var labelname = 'Lainatilasto';
+    var linkname = 'Raportti';
+    if (document.documentElement.lang.toLowerCase() === "en" ) {
+      labelname = 'Circulation statistics';
+      linkname = 'Report';
+    } else if (document.documentElement.lang.toLowerCase() === "sv-se" ) {
+      labelname = 'Utlåning statistik';
+      linkname = 'Rapport';
+    }
+
+    // Linkin luonti
+    $('#catalogue_detail_biblio div.page-section').append('<span class="results_summary"><span class="lainatiedot label">' + labelname + ': </span><a id="lainatiedot" href="#">' + linkname + '</a></span>');
+  }  
+});
+
+$('body').on('click', '#lainatiedot', function(e) {
+  e.preventDefault();
+    var reportnumber = REPLACE_BY_CONFIG_PARAM_A;
+    var reporturl = '/cgi-bin/koha/reports/guided_reports.pl?reports=' + reportnumber + '&phase=Run+this+report&param_name=biblionumber&sql_params=' + biblionumber;
+    var newin = window.open(reporturl,'popup','width=1280,height=720,resizable=no,toolbar=false,scrollbars=yes,top');
+});
+/// LOPPU ///
